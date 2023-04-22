6027e4f9d845b3f6e71f2c5b9e622d85feedd562
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-22 15:03:59 +0300

    поменяла baseurl

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.places.nomoredomains.monster/';
 
 
 const handleResponse = response => response.ok ? response.json() : Promise.reject(`Ошибка ${response.status}`)
