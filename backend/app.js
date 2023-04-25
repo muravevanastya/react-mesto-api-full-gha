c069a385d4b9d636173e2998d807d7ace25cfd78
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-25 18:04:04 +0300

    попытка настроить cors

--- backend/app.js
+++ backend/app.js
@@ -19,31 +19,31 @@ const corsConfig = {
 
 // app.use(cors({ origin: '*' }));
 
-const allowedCors = [
-  'https://places.nomoredomains.monster',
-  'https://api.places.nomoredomains.monster',
-];
+// const allowedCors = [
+//   'https://places.nomoredomains.monster',
+//   'https://api.places.nomoredomains.monster',
+// ];
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
-  }
+//     return res.end();
+//   }
 
-  return next();
-});
+//   return next();
+// });
 
 app.use(cors(corsConfig));
 
