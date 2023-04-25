b01cc0d3dc5ff0bad7102d1d4ac9a466b7eb7ee8
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-25 18:04:53 +0300

    убрала credentials для checktoken

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -39,7 +39,7 @@ export const authorize = (password, email) => {
 
 export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
-    credentials: 'include',
+    // credentials: 'include',
     method: 'GET',
     headers: {
       "Content-Type": "application/json",
